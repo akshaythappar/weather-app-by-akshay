faea2dd2c2c36485714d92dbc60c99606ce7068c
Akshay <akshaythappar9@gmail.com>
2022-06-04 14:12:25 +0530

    third commit

--- public/js/main.js
+++ public/js/main.js
@@ -5,8 +5,17 @@ const submitBtn = document.getElementById('submitBtn');
 const city_name = document.getElementById('city_name');
 const temp_real_val = document.getElementById('temp_real_val');
 const temp_status = document.getElementById('temp_status');
+const description = document.getElementById('description');
+const feels_like = document.getElementById('feels_like');
+const temp_min = document.getElementById('temp_min');
+const temp_max= document.getElementById('temp_max');
+const pressure = document.getElementById('pressure');
+const humidity = document.getElementById('humidity');
+const speed = document.getElementById('speed');
+const main = document.getElementById('main');
 
 const datahide = document.querySelector('.middle_layer');
+const datahide1 = document.querySelector('.bottom_layer');
 
 
 const getInfo = async(event)=>{
@@ -16,6 +25,7 @@ const getInfo = async(event)=>{
     if(cityVal==""){
         city_name.innerText = `Plz write the name before you search`;
         datahide.classList.add('data_hide');
+        datahide1.classList.add('data_hide');
     }
     else{
         try{ 
@@ -32,6 +42,15 @@ const getInfo = async(event)=>{
                 city_name.innerText=`${arrData[0].name},${arrData[0].sys.country}`;
                 temp_real_val.innerText = arrData[0].main.temp;
                 // temp_status.innerText = arrData[0].weather[0].main;
+                description.innerText = `Desctiption : ${arrData[0].weather[0].description}`;
+                feels_like.innerText = `feels_like : ${arrData[0].main.feels_like}`
+                temp_min.innerText = `Minimum Temperature : ${arrData[0].main.temp_min}`
+                temp_max.innerText = `Maximum Temperature : ${arrData[0].main.temp_max}`
+                pressure.innerText = `Pressure : ${arrData[0].main.pressure}`
+                humidity.innerText = `Humidity : ${arrData[0].main.humidity}`
+                speed.innerText = `Wind Speed : ${arrData[0].wind.speed} ` 
+                main.innerText = `Main : ${arrData[0].weather[0].main}`
+                
 
                 const tempMood = arrData[0].weather[0].main;
                 // console.log(tempMood);
@@ -52,11 +71,13 @@ const getInfo = async(event)=>{
                 }
 
                 datahide.classList.remove('data_hide');
+                datahide1.classList.remove('data_hide');
             
         }
         catch{
             city_name.innerText = `Plz enter the city name properly`;
             datahide.classList.add('data_hide');
+            datahide1.classList.add('data_hide');
         }
 
 
